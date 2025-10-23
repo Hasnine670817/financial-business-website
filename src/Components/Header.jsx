4263f66b79f46e1bd21017bd0845ca0e4df40b75
Hasnine670817 <hasninetorun@gmail.com>
2025-10-23 14:59:24 +0600

    header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import headerLogo from '../../public/images/logo.png'
+import { FaBars } from 'react-icons/fa';
 
 const Header = () => {
     return (
-        <header>
-            
+        <header className='py-4 bg-[#F3F4F6]/70 backdrop-blur-md sticky top-0 z-50'>
+            <div className='container-custom'>
+                <nav className='hidden lg:flex items-center justify-between'>
+                    <Link to='/'><img className='lg:max-w-[90px] 2xl:max-w-[130px]' src={headerLogo} alt="logo" /></Link>
+
+                    <ul className='flex items-center gap-10 xl:gap-14 2xl:gap-16'>
+                        <li>
+                            <NavLink to='/' className='text-sm xl:text-base 2xl:text-lg font-medium text-black relative link-a'>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/about' className='text-sm xl:text-base 2xl:text-lg font-medium text-black relative link-a'>About Us</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/services' className='text-sm xl:text-base 2xl:text-lg font-medium text-black relative link-a'>Services</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/page' className='text-sm xl:text-base 2xl:text-lg font-medium text-black relative link-a'>Page</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/blog' className='text-sm xl:text-base 2xl:text-lg font-medium text-black relative link-a'>Blog</NavLink>
+                        </li>
+                    </ul>
+
+                    <ul className='flex items-center gap-4'>
+                        <li>
+                            <Link className='text-sm xl:text-base 2xl:text-lg font-semibold text-white bg-black leading-none py-3 px-6 2xl:py-4 2xl:px-8 rounded-[26px] hover:bg-[#12300B]'>Sign Up</Link>
+                        </li>
+                        <li>
+                            <Link className='text-sm xl:text-base 2xl:text-lg font-semibold text-white bg-black leading-none py-3 px-6 2xl:py-4 2xl:px-8 rounded-[26px] hover:bg-[#12300B]'>Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className="drawer lg:hidden">
+                    <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content">
+                        {/* Page content here */}
+                        <div className='flex items-center justify-between'>
+                            <Link to='/'><img className='max-w-[60px]' src={headerLogo} alt="logo" /></Link>
+                            <label htmlFor="my-drawer-1" className="drawer-button cursor-pointer text-xl"><FaBars></FaBars></label>
+                        </div>
+                    </div>
+                    <div className="drawer-side">
+                        <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
+                        <ul className="menu bg-[#F3F4F6] min-h-full w-70 p-4">
+                            <li>
+                                <NavLink to='/' className='text-base font-medium text-black relative'>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/about' className='text-base font-medium text-black relative'>About Us</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/services' className='text-base font-medium text-black relative'>Services</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/page' className='text-base font-medium text-black relative'>Page</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/blog' className='text-base font-medium text-black relative'>Blog</NavLink>
+                            </li>
+                            <li className='mt-3'>
+                                <Link className='text-sm xl:text-base 2xl:text-lg font-semibold text-white bg-black leading-none py-3 px-6 2xl:py-4 2xl:px-8 rounded-[26px] hover:bg-[#12300B] justify-center'>Sign Up</Link>
+                            </li>
+                            <li className='mt-3'>
+                                <Link className='text-sm xl:text-base 2xl:text-lg font-semibold text-white bg-black leading-none py-3 px-6 2xl:py-4 2xl:px-8 rounded-[26px] hover:bg-[#12300B] justify-center'>Login</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
         </header>
     );
 };
